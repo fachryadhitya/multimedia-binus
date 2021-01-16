38c200f22815f4aafe6314c5ca0277532002a901
FachryAdhitya <fachryadhitya@gmail.com>
2021-01-16 22:36:28 +0700

    change footer position

--- components/StickyFooter.jsx
+++ components/StickyFooter.jsx
@@ -1,11 +1,7 @@
 const StickyFooter = () => {
   return (
     <div className="container mx-auto relative hidden xl:flex">
-      <img
-        src="/c.svg"
-        alt="crayon"
-        className="absolute lg:-bottom-6  2xl:-left-12"
-      />
+      <img src="/c.svg" alt="crayon" className="absolute lg:-bottom-6 " />
     </div>
   );
 };
